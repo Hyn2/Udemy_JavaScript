92c4acfc0c649c8cbb499d65a31106f6749aea1a
Hyn2 <justin010129@gmail.com>
2023-04-08 23:03:43 +0900

    Docs : 230408 if

---
+++ 0407/RandomNumber.js
@@ -0,0 +1,6 @@
+// 125. Random Number Generation in Javascript
+
+var randomValue = Math.floor((Math.random() * 100)) + 1;
+console.log(randomValue);
+
+
---
+++ 0408/FlowControl.js
@@ -0,0 +1,22 @@
+// 126. Control Statements: Using If-Else Conditionals & Logic
+
+// if문
+// if(condition) {
+//      code
+// } else {
+//      code
+// }
+
+// === 과 ==의 차이
+// ===는 값의 일치와 데이터 타입 일치 여부까지 비교함
+// 아래는 예제
+var a = 1;
+var b = '1';
+
+if(a === b) {
+    console.log('yes');
+} else {
+    console.log('no');
+}
+
+// 예상 출력 : no
